d13a3d36caf6d0557a771b486a78b9301e78b969
p20669 <aurel.schmid@css.ch>
2025-11-24 10:51:25 +0100

    sonar nach fix

---
+++ tools-und-automation/3-linting/1-Aufgabe/tests/__init__.py
@@ -0,0 +1,1 @@
+from ..src.house import House 
--- tools-und-automation/3-linting/1-Aufgabe/tests/test.py
+++ tools-und-automation/3-linting/1-Aufgabe/tests/test.py
@@ -1,5 +1,5 @@
 import unittest
-from src.house import *
+from ..src.house import *
 
 
 class TestHouse(unittest.TestCase):
@@ -7,6 +7,13 @@ class TestHouse(unittest.TestCase):
         house = House()
 
         self.assertIsInstance(house, House)
+
+    def test_set_name(self):
+        house = House()
+        house.set_name("villa")
+
+        print(house)
+        self.assertEqual(house.get_name(), "villa")
 
 
 if __name__ == '__main__':
